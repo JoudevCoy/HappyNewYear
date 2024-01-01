9fecf959e459f92d88559fd16216c29c6bb10735
JoudevCoy <joudev09@gmail.com>
2024-01-01 10:33:35 +0800

    #

---
+++ script.js
@@ -0,0 +1,238 @@
+var canvas = document.getElementById('canvas'),
+  ctx = canvas.getContext('2d'),
+  cw = window.innerWidth,
+  ch = window.innerHeight,
+  fireworks = [],
+  particles = [],
+  hue = 120,
+  limiterTotal = 5,
+  limiterTick = 0,
+  timerTotal = 80,
+  timerTick = 0,
+  mousedown = false,
+  mx,
+  my;
+
+canvas.width = cw;
+canvas.height = ch;
+
+window.addEventListener("resize", function(){
+  cw = window.innerWidth;
+  ch = window.innerHeight;
+  canvas.width = cw;
+  canvas.height = ch;
+});
+
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function calculateDistance(p1x, p1y, p2x, p2y) {
+  var xDistance = p1x - p2x,
+    yDistance = p1y - p2y;
+  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
+
+function Firework(sx, sy, tx, ty) {
+  this.x = sx;
+  this.y = sy;
+  this.sx = sx;
+  this.sy = sy;
+  this.tx = tx;
+  this.ty = ty;
+  this.distanceToTarget = calculateDistance(sx, sy, tx, ty);
+  this.distanceTraveled = 0;
+  this.coordinates = [];
+  this.coordinateCount = 3;
+  while (this.coordinateCount--) {
+    this.coordinates.push([this.x, this.y]);
+  }
+  this.angle = Math.atan2(ty - sy, tx - sx);
+  this.speed = 2;
+  this.acceleration = 1.2;
+  this.brightness = random(50, 100);
+  this.targetRadius = 1;
+}
+
+Firework.prototype.update = function (index) {
+  this.coordinates.pop();
+  this.coordinates.unshift([this.x, this.y]);
+
+  if (this.targetRadius < 8) {
+    this.targetRadius += 0.7;
+  } else {
+    this.targetRadius = 1;
+  }
+
+  this.speed *= this.acceleration;
+
+  var vx = Math.cos(this.angle) * this.speed,
+    vy = Math.sin(this.angle) * this.speed;
+  this.distanceTraveled = calculateDistance(this.sx, this.sy, this.x + vx, this.y + vy);
+
+  if (this.distanceTraveled >= this.distanceToTarget) {
+    createParticles(this.tx, this.ty);
+    fireworks.splice(index, 1);
+  } else {
+    this.x += vx;
+    this.y += vy;
+  }
+}
+
+Firework.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
+  ctx.lineTo(this.x, this.y);
+  ctx.strokeStyle = 'hsl(' + hue + ', 100%, ' + this.brightness + '%)';
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(this.tx, this.ty, this.targetRadius, 0, Math.PI * 2);
+  ctx.stroke();
+}
+
+// Meumbuat Partikel
+function Particle(x, y) {
+  this.x = x;
+  this.y = y;
+  this.coordinates = [];
+  this.coordinateCount = 3;
+  while (this.coordinateCount--) {
+    this.coordinates.push([this.x, this.y]);
+  }
+  this.angle = random(0, Math.PI * 4);
+  this.speed = random(1, 20);
+  this.friction = 0.95;
+  this.gravity = 3;
+  this.hue = random(hue - 50, hue + 50);
+  this.brightness = random(50, 80);
+  this.alpha = 1;
+  this.decay = random(0.015, 0.06);
+}
+
+Particle.prototype.update = function (index) {
+  this.coordinates.pop();
+  this.coordinates.unshift([this.x, this.y]);
+  this.speed *= this.friction;
+  this.x += Math.cos(this.angle) * this.speed;
+  this.y += Math.sin(this.angle) * this.speed + this.gravity;
+  this.alpha -= this.decay;
+
+  if (this.alpha <= this.decay) {
+    particles.splice(index, 1);
+  }
+}
+
+Particle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1]);
+  ctx.lineTo(this.x, this.y);
+  ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + this.alpha + ')';
+  ctx.stroke();
+}
+
+// function playFireworks(){
+//   let sfx = ["fireworks0.mp3", "fireworks1.mp3", "fireworks2.mp3"],
+//     rand = Math.floor(Math.random() * 2);
+//   const audio = new Audio();
+//   audio.src = sfx[rand];
+//   audio.play();
+// }
+
+function createParticles(x, y) {
+  // playFireworks();
+  var particleCount = 500;
+  while (particleCount--) {
+    particles.push(new Particle(x, y));
+  }
+}
+
+function loop() {
+  requestAnimationFrame(loop);
+
+  hue = random(0, 360);
+
+  ctx.globalCompositeOperation = 'destination-out';
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+  ctx.fillRect(0, 0, cw, ch);
+  ctx.globalCompositeOperation = 'lighter';
+
+  var i = fireworks.length;
+  while (i--) {
+    fireworks[i].draw();
+    fireworks[i].update(i);
+  }
+
+  var i = particles.length;
+  while (i--) {
+    particles[i].draw();
+    particles[i].update(i);
+  }
+
+  if (timerTick >= timerTotal) {
+    if (!mousedown) {
+      fireworks.push(new Firework(cw / 2, ch, random(0, cw), random(0, ch / 2)));
+      timerTick = 0;
+    }
+  } else {
+    timerTick++;
+  }
+
+  if (limiterTick >= limiterTotal) {
+    if (mousedown) {
+      fireworks.push(new Firework(cw / 2, ch, mx, my));
+      limiterTick = 0;
+    }
+  } else {
+    limiterTick++;
+  }
+}
+
+canvas.addEventListener('mousemove', function (e) {
+  mx = e.pageX - canvas.offsetLeft;
+  my = e.pageY - canvas.offsetTop;
+});
+
+canvas.addEventListener('mousedown', function (e) {
+  e.preventDefault();
+  mousedown = true;
+});
+
+canvas.addEventListener('mouseup', function (e) {
+  e.preventDefault();
+  mousedown = false;
+});
+
+const video = document.getElementById("video"),
+  loadingOverlay = document.querySelector(".loading-overlay");
+video.addEventListener("loadeddata", async function(){
+  loadingOverlay.addEventListener("click", await function(){
+    loadingOverlay.children[0].innerHTML = "Tunggu sebentar";
+    video.play();
+    video.addEventListener("playing", function(){
+      showDur = 6.2 * 1000;
+      setTimeout(() => {
+        loadingOverlay.style.animation = "transition 1s linear forwards";
+      }, showDur);
+    });
+    
+    loop();
+  });
+});
+
+let shareBtn = document.getElementById("shareBtn"),
+  shareData = {
+    title: "Happy New Year 2023 -> 2024",
+    text: "Happy New Year!! Selamat Tahun Baru!! \n lihat kejutan yang saya kirim!",
+    url: window.location.href
+  }
+
+async function shareFunc() {
+  console.log(shareData);
+  try {
+    await window.navigator.share(shareData);
+  } catch (err) {
+    alert("Something error!");
+    console.log(err);
+  }
+};
